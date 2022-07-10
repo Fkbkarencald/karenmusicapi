5e39b7d51587b7feaf4671de827a90e11df2e1cc
Frankie <Frankie.benjamin1995@gmail.com>
2022-07-10 15:04:06 +1000

    create table query

---
+++ create tables.sql
@@ -0,0 +1,11 @@
+create table artist (
+	artist_id int NOT NULL auto_increment,
+    firstName varchar(255) NOT NULL,
+    karenName varchar(255),
+    showName varchar(255),
+    LastName varchar(255),
+    imageURL varchar(255),
+    
+    primary key(artist_id)
+);
+
